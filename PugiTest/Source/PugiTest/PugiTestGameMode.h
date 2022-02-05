9d8947a9dddd01bd30ada92c934ff4f1a5ce7d21
bogguser <glinbb@gmail.com>
2022-02-05 17:46:29 +0300

    Task2

---
+++ PugiTest/Source/PugiTest/PugiTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PugiTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APugiTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APugiTestGameMode();
+};
+
+
+
