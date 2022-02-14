e8910b515bfb0ce19a831c9735886a81e5f2c1fd
bogguser <glinbb@gmail.com>
2022-02-14 19:44:53 +0300

    123

---
+++ task3/Source/PugiTest/PugiTest.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PugiTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PugiTest, "PugiTest" );
+ 
