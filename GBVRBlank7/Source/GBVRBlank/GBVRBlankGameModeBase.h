9676478d2a783d71b9a485ccc7194987ba70dd64
bogguser <glinbb@gmail.com>
2022-06-07 18:47:54 +0300

    task7

---
+++ GBVRBlank7/Source/GBVRBlank/GBVRBlankGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GBVRBlankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GBVRBLANK_API AGBVRBlankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
